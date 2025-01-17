06eebc1dfa98e5c8d0981846714182fe13bc505e
TomaszLizer <tomasz.lizer@anywhere.co>
2025-01-17 07:22:33 +0100

    Initial Commit

---
+++ Trivia-Quiz/Trivia_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_QuizApp.swift
+//  Trivia-Quiz
+//
+//  Created by Tomasz Lizer on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trivia-QuizTests/Trivia_QuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_QuizTests.swift
+//  Trivia-QuizTests
+//
+//  Created by Tomasz Lizer on 17/01/2025.
+//
+
+import Testing
+@testable import Trivia_Quiz
+
+struct Trivia_QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
